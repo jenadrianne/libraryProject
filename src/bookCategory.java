cc3ebf8dbf78512e00ea76f4a20d15a509b7830b
Jennifer Adrianne I. Caballes <yan4414@gmail.com>
2021-07-27 22:34:44 +0800

    Add files via upload

---
+++ src/bookCategory.java
@@ -0,0 +1,10 @@
+
+public enum bookCategory {
+	//non-fiction category
+	SCIENCE,
+	HISTORY, 
+	BIOGRAPHY, 
+	//fiction category
+	COMIC, 
+	FANTASY
+}
---
+++ src/bookGenre.java
@@ -0,0 +1,5 @@
+
+public enum bookGenre {
+	FICTION,
+	NONFICTION
+}
